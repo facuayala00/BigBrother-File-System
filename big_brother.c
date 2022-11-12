23a1d04b93ad510b056fdf7a79c0e3a36bbaaca3
gastonBon <gastonbonfils@gmail.com>
2022-11-12 20:06:36 -0300

    ojo que se viene parte 2

--- big_brother.c
+++ big_brother.c
@@ -25,23 +25,43 @@ u32 search_bb_orphan_dir_cluster() {
     u32 bb_dir_start_cluster = 2; //2 no 0, ver next_free
     fat_volume vol = get_fat_volume();
     u32 max_clusters = vol->table->num_data_clusters; //funciona para no iterar de mas
+    // DEBUG("MAS CLUSTERS %u", max_clusters);
 
     while ( bb_dir_start_cluster < max_clusters){ //faltaria checkear si es el dir correcto
-        if (le32_to_cpu(((const le32 *)vol->table->fat_map)[bb_dir_start_cluster]) != FAT_CLUSTER_BAD_SECTOR) {
+        if (fat_table_cluster_is_bad_sector(le32_to_cpu(((const le32 *)vol->table->fat_map)[bb_dir_start_cluster]))) {
+            DEBUG("TENGO BAD SECTOR");
+            DEBUG("BAD SECTOR %u", bb_dir_start_cluster);
             u32 bytes_per_cluster = fat_table_bytes_per_cluster(vol->table);
             off_t offset = fat_table_cluster_offset(vol->table, bb_dir_start_cluster);
             u8 *buf = alloca(bytes_per_cluster);
             full_pread(vol->table->fd, buf, bytes_per_cluster, offset);
 
-            if(bb_is_log_file_dentry((fat_dir_entry) buf)) {
+            fat_dir_entry dentry = (fat_dir_entry) buf;
+            // DEBUG("TIENE NOMBRECITO");
+            // DEBUG("%s",dentry->base_name );
+            if(bb_is_log_file_dentry(dentry)) {
+                DEBUG("TIENE NOMBRECITO EN CLUSTER %u", bb_dir_start_cluster);
+                DEBUG("NOMBRE: %s",dentry->base_name );
+
+                // free(buf);
                 break;
             }   
+            // break;
         }
         bb_dir_start_cluster++;
+        // DEBUG("chiquitin %u", bb_dir_start_cluster);
+
+        if (bb_dir_start_cluster > 10000) {
+            DEBUG("SE FUE LEJOS EL BB");
+            bb_dir_start_cluster = 0;
+            break;
+        }
+        
     }
-    if (bb_dir_start_cluster >= max_clusters) {
-        bb_dir_start_cluster = 0;
-    }
+    // if (bb_dir_start_cluster >= max_clusters) {
+    //     DEBUG("SE FUE LEJOS EL BB");
+    //     bb_dir_start_cluster = 0;
+    // }
 
     return bb_dir_start_cluster;
 }
@@ -63,17 +83,22 @@ int bb_init_log_dir(u32 start_cluster) {
     fat_volume vol = NULL;
     fat_tree_node root_node = NULL;
 
-
-
     vol = get_fat_volume();
-    start_cluster = fat_table_get_next_free_cluster(vol->table);
-    // Create a new file from scratch, instead of using a direntry like normally done.
-    fat_file loaded_bb_dir = fat_file_init_orphan_dir(BB_DIRNAME, vol->table, start_cluster);
-    start_cluster = FAT_CLUSTER_BAD_SECTOR;
 
-    // Add directory to file tree. It's entries will be like any other dir.
-    root_node = fat_tree_node_search(vol->file_tree, "/");
-    vol->file_tree = fat_tree_insert(vol->file_tree, root_node, loaded_bb_dir);
+    if(start_cluster == 0) {
+        start_cluster = fat_table_get_next_free_cluster(vol->table);
+        DEBUG("%u TOMO ESTE CLUSTER LIBRE", start_cluster);
+
+        // Create a new file from scratch, instead of using a direntry like normally done.
+        fat_file loaded_bb_dir = fat_file_init_orphan_dir(BB_DIRNAME, vol->table, start_cluster);
+        fat_table_set_next_cluster(vol->table, start_cluster, FAT_CLUSTER_BAD_SECTOR);
+        DEBUG("%u CHIQUITAAAAA", start_cluster);
+
+        // Add directory to file tree. It's entries will be like any other dir.
+        root_node = fat_tree_node_search(vol->file_tree, "/");
+        vol->file_tree = fat_tree_insert(vol->file_tree, root_node, loaded_bb_dir);
+    }
+    
 
     return -errno;
 }
--- fat_fuse_ops.c
+++ fat_fuse_ops.c
@@ -213,7 +213,7 @@ int fat_fuse_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
     child = children;
     while (*child != NULL) {
         // Hide fs.log from ls
-        if (!fat_file_cmp_path(*child, BB_DIRNAME) == 0) {
+        if (!fat_file_cmp_path(*child, "myaa") == 0) {
             error = (*filler)(buf, (*child)->name, NULL, 0);
             if (error != 0) {
                 free(children);
